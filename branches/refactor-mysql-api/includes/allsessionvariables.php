8a19f70a8a2c1ce312fd876d259cb25f0b06796e
kbriggs <kbriggs@419b91d4-f556-4edb-ac5d-3728f83554c7>
2017-01-10 02:49:31 +0000

    Update allsessionvariables.php to mysqli API

--- branches/refactor-mysql-api/includes/allsessionvariables.php
+++ branches/refactor-mysql-api/includes/allsessionvariables.php
@@ -5,19 +5,19 @@ $_SESSION['came_from'] = trim(strtok($_SESSION['came_from'], '?'));
 
 $divisions = array();
 require_once ('../mysql_connect.php'); //Connect to the db.
-$query = "SELECT * from divisions ORDER BY div_name";
-$result = mysql_query($query);
-while ($row = mysql_fetch_assoc($result)) {
+$query = "SELECT * FROM divisions ORDER BY div_name";
+$result = mysqli_query($dbc, $query);
+while ($row = mysqli_fetch_assoc($result)) {
 	$divisions[$row['div_link']] = $row['div_name'];
-	}
+}
 	
 if(isset($_SESSION['this_emp_id'])){
 	$this_emp_id = $_SESSION['this_emp_id'];
 	$this_assignment_id = $_SESSION['this_assignment_id'];
 	$query = "SELECT first_name, last_name, name_dup from employees WHERE emp_id = '$this_emp_id'";
-	$result = mysql_query($query);
-	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)){
+	$result = mysqli_query($dbc, $query);
+	while ($row = mysqli_fetch_assoc($result)){
 		$this_full_name = $row['first_name'].' '.$row['last_name'];
-		}
 	}
+}
 ?>
