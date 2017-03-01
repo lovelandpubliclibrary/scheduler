10afd834fc6f38c552eee997facf5dac8b08f69d
kbriggs <kbriggs@419b91d4-f556-4edb-ac5d-3728f83554c7>
2017-03-01 17:00:43 +0000

    cleanup extraneous files in branch

--- branches/refactor-mysql-api/block_daily_schedule.php
+++
@@ -1,9 +0,0 @@
-<?php #block2.php
-date_default_timezone_set('America/Denver');
-require_once ('../mysql_connect.php');
-include('./includes/allsessionvariables.php');
-include ('./display_functions.php');
-$today = $_GET['today'];
-$now = strtotime($today);
-daily_schedule($now, $divisions);
-?>
--- branches/refactor-mysql-api/includes/mobiledivdrop.php
+++
@@ -1,6 +0,0 @@
-<?php #mobiledivdrop.php
-
-foreach ($divisions as $k=>$v){
-	echo '<li><a href="/scheduler2/'.$k.'/daily">'.$v.'</a></li>';
-	}
-?>
