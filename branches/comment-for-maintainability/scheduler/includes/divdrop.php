9c9f33d8825a2b3cd45581e741087bc680ba878a
kbriggs <kbriggs@419b91d4-f556-4edb-ac5d-3728f83554c7>
2017-03-08 17:06:52 +0000

    remove comment-for-maintainability branch

--- branches/comment-for-maintainability/scheduler/includes/divdrop.php
+++
@@ -1,7 +0,0 @@
-<?php #divdrop.php
-
-foreach ($divisions as $k=>$v){
-	echo '<a href="/scheduler/'.$k.'/daily">'.$v.'</a>';
-	}
-	
-?>
--- branches/comment-for-maintainability/scheduler/includes/mobiledivdrop.php
+++
@@ -1,6 +0,0 @@
-<?php #mobiledivdrop.php
-
-foreach ($divisions as $k=>$v){
-	echo '<li><a href="/scheduler/'.$k.'/daily">'.$v.'</a></li>';
-	}
-?>
