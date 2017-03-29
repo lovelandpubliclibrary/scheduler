4bd1d8c37e45d418cb0dd8a607314e629a386c00
kbriggs <kbriggs@419b91d4-f556-4edb-ac5d-3728f83554c7>
2017-03-29 19:15:27 +0000

    use mysql_connect.php instead of mysql_connect_sched2.php for database connections

--- trunk/scheduler/includes/allsessionvariables.php
+++ trunk/scheduler/includes/allsessionvariables.php
@@ -4,7 +4,7 @@ $_SESSION['came_from'] = substr($from, strrpos($from,'/')+1);
 $_SESSION['came_from'] = trim(strtok($_SESSION['came_from'], '?'));
 
 $divisions = array();
-require_once ('../mysql_connect_sched2.php'); //Connect to the db.
+require_once ('../mysql_connect.php'); //Connect to the db.
 $query = "SELECT * from divisions ORDER BY div_name";
 $result = mysql_query($query);
 while ($row = mysql_fetch_assoc($result)) {
