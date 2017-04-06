9d6a40efc8210c21f96354d4de1465a280ee9a6e
Kevin Briggs <redisforlosers@gmail.com>
2017-04-06 11:46:30 -0600

    modify pdf_generator.php to be used regardless of dev/live/testing by using relative paths

--- scheduler/pdf_generator.php
+++ scheduler/pdf_generator.php
@@ -3,8 +3,8 @@
 require_once("dompdf/dompdf_config.inc.php");
 
 ob_start();
-echo '<link rel="stylesheet" type="text/css" media="all" href="C:/Apache/htdocs/scheduler/style/dompdf.css" />';
-echo '<link rel="stylesheet" type="text/css" media="all" href="C:/Apache/htdocs/scheduler/style/scheduler_tables.css" />';
+echo '<link rel="stylesheet" type="text/css" media="all" href="style/dompdf.css" />';
+echo '<link rel="stylesheet" type="text/css" media="all" href="style/scheduler_tables.css" />';
 date_default_timezone_set('America/Denver');
 $today = date('Y-m-d');
 $day = date('j');
@@ -12,7 +12,8 @@ $month = date('F');
 $year = date('Y');
 
 $page_title = "$day $month $year";
-include ('C:/Apache/htdocs/scheduler/display_functions.php');
+//include ('C:/Apache/htdocs/scheduler/display_functions.php');
+include ('display_functions.php');
 
 require_once ('../mysql_connect.php'); //Connect to the db.
 $divisions = array();
@@ -39,8 +40,11 @@ $old_limit = ini_set("memory_limit", "192M");
 $dompdf = new DOMPDF();
 $dompdf->set_base_path('/'); 
 $dompdf->load_html($html);
+echo "<tt><pre>";
+var_dump($dompdf->output_html());
+echo "<tt><pre>";
 $dompdf->set_paper('legal', 'portrait');
 $dompdf->render();
-file_put_contents("C:/Apache/htdocs/scheduler/archives/$today.pdf", $dompdf->output());
+file_put_contents("archives/$today.pdf", $dompdf->output());
 
 ?>
