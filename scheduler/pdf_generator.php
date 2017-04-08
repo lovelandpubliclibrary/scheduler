9d58901ffa5cd7dee270279423783d2a40d4bbff
Kevin Briggs <redisforlosers@gmail.com>
2017-04-08 16:45:50 -0600

    finalize changes to pdf_generator.php for testing

--- scheduler/pdf_generator.php
+++ scheduler/pdf_generator.php
@@ -1,10 +1,10 @@
 <?php #pdf_generator.php
 
-require_once("dompdf/dompdf_config.inc.php");
+require_once("/home/teulberg/lpl-repository.com/scheduler/dompdf/dompdf_config.inc.php");
 
 ob_start();
-echo '<link rel="stylesheet" type="text/css" media="all" href="style/dompdf.css" />';
-echo '<link rel="stylesheet" type="text/css" media="all" href="style/scheduler_tables.css" />';
+echo '<link rel="stylesheet" type="text/css" media="all" href="/home/teulberg/lpl-repository.com/scheduler/style/dompdf.css" />';
+echo '<link rel="stylesheet" type="text/css" media="all" href="/home/teulberg/lpl-repository.com/scheduler/style/scheduler_tables.css" />';
 date_default_timezone_set('America/Denver');
 $today = date('Y-m-d');
 $day = date('j');
@@ -12,10 +12,8 @@ $month = date('F');
 $year = date('Y');
 
 $page_title = "$day $month $year";
-//include ('C:/Apache/htdocs/scheduler/display_functions.php');
-include ('display_functions.php');
+include ('/home/teulberg/lpl-repository.com/scheduler/display_functions.php');
 
-require_once ('../mysql_connect.php'); //Connect to the db.
 $divisions = array();
 require_once ('../mysql_connect.php'); //Connect to the db.
 $query = "SELECT * from divisions ORDER BY div_name";
@@ -42,6 +40,6 @@ $dompdf->set_base_path('/');
 $dompdf->load_html($html);
 $dompdf->set_paper('legal', 'portrait');
 $dompdf->render();
-file_put_contents("archives/$today.pdf", $dompdf->output());
+file_put_contents("/home/teulberg/lpl-repository.com/scheduler/archives/$today.pdf", $dompdf->output());
 
 ?>
